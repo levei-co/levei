91aea0de9117ea25c41dd882d623387a3f1754f6
Leo <leo.spaula@gmail.com>
2020-06-30 19:58:31 -0300

    Add Dashboard controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users,
+    path: '', # optional namespace or empty string for no space
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      registration: 'register',
+      sign_up: 'signup'
+    }
+
+
   root 'home#index'
+  namespace :dashboard do
+    get "/" => "dashboard#index"
+  end
   mount Sidekiq::Web => '/sidekiq'
 end
